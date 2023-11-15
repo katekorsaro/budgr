a017e03fe1279ef9069b0a8acacc466a95cf2dd3
Kate Korsaro <kate.korsaro@gmail.com>
2023-11-15 16:38:32 +0100

    WIP: Wed 15/11/2023 16:38

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 /// version 0.0.1
+
 /// GIVEN a CSV file with 3 fields
-/// THEN I want to print read it line be line and print it on stdout
+/// THEN I want to print read it line by line and print it on stdout
+
+use std::fs;
+
+fn print_file_content(c:&String){
+    print!("{c}");
+}
+
 fn main() {
-    todo!();
+    let r = fs::read_to_string("test.csv");
+
+    match r {
+        Err(err) => println!("ERR: {err}"),
+        Ok(result) => {
+            print_file_content(&result);
+        }
+    }
 }
