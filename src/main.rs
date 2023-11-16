a790b57b10a3b39a43c414ea4d1a518de0e55f85
Kate Korsaro <kate.korsaro@gmail.com>
2023-11-16 13:11:34 +0100

    WIP: Thu 16/11/2023 13:11

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@
 
 /// GIVEN a CSV file with 3 fields
 /// THEN I want to print read it line by line and print it on stdout
+use std::fs::File;
+use std::io::{BufRead,BufReader};
 
-use std::fs;
+fn main() -> std::io::Result<()> {
+    let f = File::open("data/test.csv")?;
+    let mut r = BufReader::new(f);
 
-fn print_file_content(c:&String){
-    print!("{c}");
-}
+    loop {
+        let mut line:String = String::new();
+        let result = r.read_line(&mut line);
 
-fn main() {
-    let r = fs::read_to_string("test.csv");
-
-    match r {
-        Err(err) => println!("ERR: {err}"),
-        Ok(result) => {
-            print_file_content(&result);
+        match result {
+            Ok(0) => break,
+            _ => print!("{line}"),
         }
     }
+    Ok(())
 }
