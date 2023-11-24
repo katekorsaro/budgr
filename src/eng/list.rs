a72a843be68aaf7214628ea40d185e8bc8ff03bf
Kate Korsaro <kate.korsaro@gmail.com>
2023-11-24 09:15:36 +0100

    WIP: Fri 24/11/2023 09:15

---
+++ src/eng/list.rs
@@ -0,0 +1,71 @@
+use crate::obj::op::Op;
+use crate::eng::{EngErr};
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader,BufRead};
+
+pub fn list (filename: &str) -> Result<Vec<Op>, EngErr> {
+    let path = Path::new(filename);
+
+    if path.exists() == false {
+        return Err(EngErr::DataFileNotFound);
+    }
+
+    let file = File::open(filename);
+    let file = match file {
+        Err(_) => return Err(EngErr::DataFileNotReadable),
+        Ok(f)=> f,
+    };
+
+    let mut ret = Vec::<Op>::new();
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => ret.push(Op::from(&line)),
+            Err(_) => return Err(EngErr::WhileReadingFile),
+        }
+    }
+
+    Ok(ret)
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn list_call () {
+        let _ = super::list("./testdata/db.csv");
+    }
+
+    #[test]
+    fn list_file_not_found () {
+        let result = super::list("./testdata/non_existing_db.csv");
+        assert_eq!(result, Err(super::EngErr::DataFileNotFound));
+    }
+
+    #[test]
+    fn list_row_number () {
+        let result = super::list("./testdata/db.csv");
+        match result {
+            Ok(ops) => assert_eq!(ops.len(), 30),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn list_first_row () {
+        let result = super::list("./testdata/db.csv");
+        match result {
+            Err(_) => assert!(false),
+            Ok(ops) => {
+                assert_eq!(ops[0].date, 20230101);
+                assert_eq!(ops[0].val, 1000);
+                assert_eq!(ops[0].desc, String::from("Description for operation 001"));
+
+                assert_eq!(ops[15].date, 20230116);
+                assert_eq!(ops[15].val, -1000);
+                assert_eq!(ops[15].desc, String::from("Description for operation 001"));
+            },
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,4 @@ mod eng;
 mod obj;
 
 fn main () {
-    let ops = eng::list("./data/db.csv");
-
-    match ops {
-        Err(_) => panic!(),
-        Ok(ops) => {
-            for op in ops {
-                println!("{op:?}");
-            }
-        }
-    }
 }
