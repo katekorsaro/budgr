dfabd6fff0b6efb6e2ac1e320964815aeb0508ac
Kate Korsaro <kate.korsaro@gmail.com>
2023-11-28 16:54:06 +0100

    WIP: Tue 28/11/2023 16:54

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,22 @@ fn main () {
     let ops = eng::list("./data/db.csv");
 
     match ops {
-        Err(_) => panic!(),
-        Ok(ops) => ops.iter().for_each(|op| println!("{op:?}")),
+        Err(ref msg) =>  println!("{msg:?}"),
+        Ok(ref ops) =>  print_ops(ops),
     }
+
+    println!("{ops:?}");
+}
+
+fn print_ops (ops: &Vec<obj::Op>) {
+    let ops = ops.iter()
+        .filter(|x| x.date <= 20230110)
+        .map(|x| x.date);
+
+    let ops = ops
+        .rev()
+        .take(3);
+
+    ops
+        .for_each(|x| println!("{x}"));
 }
