2b8b1ab99233bbdc0568a9755b41b879d4f0dbe0
Kate Korsaro <kate.korsaro@gmail.com>
2023-11-29 14:08:38 +0100

    WIP: Wed 29/11/2023 14:08

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,22 @@ fn main () {
 
     match ops {
         Err(ref msg) =>  println!("{msg:?}"),
-        Ok(ref ops) =>  print_ops(ops, Some(20230110)),
+        Ok(ref ops) =>  print_ops(ops, Some(20230110), Some("New description...")),
     }
 }
 
-fn print_ops (ops: &[obj::Op], from_date:Option<u32>) {
+fn print_ops (ops: &[obj::Op], from_date:Option<u32>, desc:Option<&str>) {
     let ops = ops.iter();
 
+    // filter
     let ops = ops.filter(|x| x.date >= from_date.unwrap_or(0));
 
+    // map
+    let ops = ops.map(|x| obj::Op {
+        desc: String::from(desc.unwrap_or(&x.desc)),
+        ..*x
+    });
+
+    // print
     ops.for_each(|x| println!("{x:?}"));
 }
