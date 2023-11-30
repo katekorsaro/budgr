c6e91372f876ba8a685d9902d011434759b8c0cf
Kate Korsaro <kate.korsaro@gmail.com>
2023-11-30 12:13:09 +0100

    WIP: Thu 30/11/2023 12:13

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use budgr::list;
 use std::env;
+use cli::*;
 
 mod cli;
 
@@ -7,23 +7,8 @@ fn main() {
     let args:Vec<String> = env::args().collect();
 
     match args.len() {
-        0 => show_help(),
+        1 => show_help(),
         _ => handle_command(args),
     }
 }
 
-fn show_help () {
-    todo!();
-}
-
-fn handle_command(args:Vec<String>) {
-    let cmd = match args[1].as_str() {
-        "ls" => "ls",
-        "list" => "ls",
-        _ => "nop",
-    };
-
-    println!("{cmd}");
-
-    todo!();
-}
