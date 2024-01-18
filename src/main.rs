a68db1054411d61d3beab3c873b41e234692f5d1
Kate Korsaro <kate.korsaro@gmail.com>
2024-01-18 11:50:55 +0100

    clap integrated and first command 'list' parsed

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
+use clap::{Parser,Subcommand};
+
+#[derive(Parser)]
+#[command()]
+struct Budgr {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all operations
+    List,
+}
+
 fn main () {
+    let cmd = Budgr::parse();
 }
