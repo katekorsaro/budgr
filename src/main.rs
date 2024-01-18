4a252436d55b8d14fc3a6b771fa913d2b045300a
Kate Korsaro <kate.korsaro@gmail.com>
2024-01-18 14:39:27 +0100

    read from config data directory option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::env;
-use clap::{Parser,Subcommand};
+use clap::{Parser, Subcommand};
+use std::{env, fs};
 
 #[derive(Parser)]
 #[command()]
@@ -14,18 +14,47 @@ enum Commands {
     List,
 }
 
-fn main () {
+#[derive(Debug, Default)]
+struct Config {
+    pub data: String,
+}
+
+fn main() {
     // read env variable to get configuration
-    let budgrrc = env::vars()
-        .find(|(k, _)| k == "BUDGRRC" );
+    let budgrrc = env::vars().find(|(k, _)| k == "BUDGRRC");
 
     // store env variable into an actual variable
     let budgrrc = match budgrrc {
         Some((_, value)) => value,
-        None => String::from(".//.budgrrc//"),
+        None => String::from(".//.budgrrc"),
     };
 
-    println!("{budgrrc}");
+    let config = read_configuration(&budgrrc);
+    println!("{config:?}");
 
     let _cmd = Budgr::parse();
+}
+
+fn read_configuration(filename: &str) -> Config {
+    let mut retvalue = Config::default();
+
+    // try to read configuration file
+    let config = fs::read_to_string(filename).unwrap();
+    config
+        .lines()
+        .map(|x| {
+            let mut parts = x.split('=');
+            (parts.next().unwrap(), parts.next())
+        })
+        //handling single config keys
+        .for_each(|x| match x.0 {
+            "budgr.data" => {
+                if let Some(value) = x.1 {
+                    retvalue.data = String::from(value);
+                }
+            }
+            _ => unreachable!("All configurations should be covered for now."),
+        });
+
+    retvalue
 }
