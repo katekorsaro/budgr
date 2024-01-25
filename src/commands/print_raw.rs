6187538006106a0b5bf63be32d63e88d640bbfa7
Kate Korsaro <kate.korsaro@gmail.com>
2024-01-25 16:09:05 +0100

    fix for creation date now showing

--- src/commands/print_raw.rs
+++ src/commands/print_raw.rs
@@ -3,15 +3,7 @@ use crate::data::Data;
 pub fn print_raw(data: Vec<Data>) {
   data.into_iter().for_each(|operation| {
     println!(
-      "{}|{}|{}|{}|{}|{}|{} (file: {})",
-      operation.id,
-      operation.date,
-      operation.note,
-      operation.amount,
-      operation.account.unwrap_or("".to_string()),
-      operation.purpose.unwrap_or("".to_string()),
-      operation.goal.unwrap_or("".to_string()),
-      operation.path
+        "{}", operation.to_raw_string()
     );
   });
 }
