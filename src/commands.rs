ea9dd8b16fc2e6114f89b7d2104e0bfff6bbf412
Kate Korsaro <kate.korsaro@gmail.com>
2024-01-25 08:41:44 +0100

    wip: path information for raw output

--- src/commands.rs
+++ src/commands.rs
@@ -68,14 +68,15 @@ fn prettify_date(date: u32) -> String {
 fn print_raw(data: Vec<Data>) {
   data.into_iter().for_each(|operation| {
     println!(
-      "{}|{}|{}|{}|{}|{}|{}",
+      "{}|{}|{}|{}|{}|{}|{} (file: {})",
       operation.id,
       operation.date,
       operation.note,
       operation.amount,
       operation.account.unwrap_or("-".to_string()),
       operation.purpose.unwrap_or("-".to_string()),
-      operation.goal.unwrap_or("-".to_string())
+      operation.goal.unwrap_or("-".to_string()),
+      operation.path
     );
   });
 }
