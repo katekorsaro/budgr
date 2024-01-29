897742b58b5e1de348db8069fd0394b0963f42dc
Kate Korsaro <kate.korsaro@gmail.com>
2024-01-29 12:11:15 +0100

    cargo format

--- src/commands/undelete_operations.rs
+++ src/commands/undelete_operations.rs
@@ -1,7 +1,7 @@
+use crate::commands::set_operation_status::set_operation_status;
 use crate::data::Status;
 use crate::Budgr;
 use crate::Config;
-use crate::commands::set_operation_status::set_operation_status;
 
 pub fn undelete_operations(config: &Config, args: &Budgr) {
   set_operation_status(config, args, Status::Active);
