483a539b81dd8c0de4bc44e2d66b3cf04ded523b
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-01 09:48:24 +0100

    refactor status into separate module

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,33 +1,11 @@
+mod status;
+
 use crate::Config;
 use std::fs;
 use std::path::Path;
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq, Copy, Clone)]
-pub enum Status {
-  Active,
-  Deleted,
-}
-
-impl FromStr for Status {
-  type Err = String;
-  fn from_str(value: &str) -> Result<Self, String> {
-    match value {
-      "active" => Ok(Status::Active),
-      "deleted" => Ok(Status::Deleted),
-      _ => Err(format!("value '{}' not mapped", value)),
-    }
-  }
-}
-
-impl ToString for Status {
-  fn to_string(&self) -> String {
-    match self {
-      Status::Active => String::from("active"),
-      Status::Deleted => String::from("deleted"),
-    }
-  }
-}
+pub use status::*;
 
 #[derive(Debug)]
 pub struct Operation {
